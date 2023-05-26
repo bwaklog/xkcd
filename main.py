f6c21ad47bc55361811ebb0ae0c2255572f74d17
bwaklog <adityamhegde@icloud.com>
2023-05-27 00:27:35 +0530

    v1.1

--- main.py
+++ main.py
@@ -24,6 +24,28 @@ def get_latest_comic(verbose):
             return comic - 1
 
 
+class Comic:
+    def __init__(self, comic):
+        self.comic = comic
+
+        self.num = comic['num']
+        self.link = comic['link']
+        self.year = comic['year']
+        self.alt = comic['alt']
+        self.img = comic['img']
+        self.title = comic['title']
+
+    def display(self):
+        display_text = f"""
+XKCD Comic {self.num}
+    > year {self.year}
+    > title {self.title}
+    > comic {self.img}
+    > alt {self.alt}
+        """
+        print(display_text)
+
+
 def get_comic(verbose=False, comic="latest"):
     latest_comic = get_latest_comic(verbose)
     if comic.isdigit() and int(comic) > latest_comic:
@@ -32,35 +54,30 @@ def get_comic(verbose=False, comic="latest"):
     if comic == "latest":
         comic_request = requests.get(
             f"https://xkcd.com/{str(latest_comic)}/info.0.json")
-        return json.dumps(comic_request.json(), indent=4)
+        comic = Comic(comic_request.json())
+        comic.display()
+        return "👋🐍 hisss."
+
     else:
         comic_number = str(comic)
         comic_request = requests.get(
             f"https://xkcd.com/{comic_number}/info.0.json")
-        return json.dumps(comic_request.json(), indent=4)
+        comic = Comic(comic_request.json())
+        comic.display()
+
+        return "👋🐍 hisss."
 
 
 if __name__ == "__main__":
-    comic = "latest"
-    verbose = False
-    args = sys.argv[1:]
-    if len(args) > 1:
-        if args[1] in ['-v', '--verbose']:
-            verbose = True
-        else:
-            verbose = False
+    verbose, comic = False, "latest"
+    flags = sys.argv[1:]
+    print(flags)
+    if flags != []:
+        if flags[0] in ['-l', '--latest']:
+            print("🚀 Getting latest comic")
+            print(get_comic(verbose, comic))
+        elif flags[0].isdigit():
+            print(f"🔎 Fetching comic {flags[0]}")
+            print(get_comic(verbose, flags[0]))
     else:
-        verbose = False
-
-    if args[0]:
-        if args[0] in ['-l', '--latest']:
-            comic = "latest"
-        elif args[0].isdigit():
-            comic = args[0]
-    else:
-        print(
-            "Please specify a comic number or use the -l\
-            (or --latest) flag to get the latest comic"
-        )
-
-    print(get_comic(verbose=verbose, comic=comic))
+        print("No flags passed")
